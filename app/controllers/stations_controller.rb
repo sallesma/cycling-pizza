dcd482fd2e4ce8aa0359763e06f1ad93179df2ee
Martin Salles <salles.martin@gmail.com>
2017-01-25 18:20:32 +0100

    Faster view

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -3,6 +3,7 @@ class StationsController < ApplicationController
   def index
     contract_name = params[:contract_name] || 'Paris'
     @stations = Station.where(contract_name: contract_name)
+    @station_statuses_counts = StationStatus.group(:station_id).count
   end
 
 end
