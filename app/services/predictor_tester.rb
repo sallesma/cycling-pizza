e90dd0d56b6d5846bab395baa0d7d7e766d02d35
Martin Salles <salles.martin@gmail.com>
2017-02-21 17:21:59 +0100

    Add class to test predictor efficiency

---
+++ app/services/predictor_tester.rb
@@ -0,0 +1,21 @@
+class PredictorTester
+
+  def test_predictor(predictor, iterations = 100)
+    stations = stations_cohort(iterations)
+
+    deviations = stations.map { |station| prediction_deviation(station, predictor) }
+    deviations.reduce(:+) / deviations.size
+  end
+
+  private
+
+  def prediction_deviation(station, predictor)
+    last_status = station.station_statuses.order(:last_update_at).last
+    prediction = predictor.predict(station, last_status.last_update_at)
+    (prediction.available_bikes - last_status.available_bikes).to_f.abs
+  end
+
+  def stations_cohort(iterations)
+    Station.order('RANDOM()').first(iterations)
+  end
+end
